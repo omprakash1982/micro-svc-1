c5f5fbd36e84e6ff7853ede3f69013e65ff5ee33
omprakash <op4407@gmail.com>
2022-04-29 19:44:53 +0530

    cloud-gateway

---
+++ cloud-gateway/src/main/java/com/practicwithdcb/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.practicwithdcb.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableConfigServer
+
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
---
+++ cloud-gateway/src/test/java/com/practicwithdcb/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicwithdcb.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
