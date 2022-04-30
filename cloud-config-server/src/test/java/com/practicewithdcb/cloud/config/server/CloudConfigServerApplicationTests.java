9f2e81ee8741af8a6a0927bdebcdc329bb321ee0
omprakash <op4407@gmail.com>
2022-04-30 14:31:39 +0530

    first-commit

---
+++ cloud-config-server/src/test/java/com/practicewithdcb/cloud/config/server/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicewithdcb.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
